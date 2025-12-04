8f66b70ad00ecb59e0ba9d6cc65fdb1ba0e25aff
Scott Hillman <hillmanov@gmail.com>
2025-12-04 15:17:01 -0700

    Massive upgreade for day 2.2

--- day02/index.ts
+++ day02/index.ts
@@ -35,74 +35,43 @@ async function part2(): Promise<number> {
   const invalidIDs : number[] = []
 
   each(input, ({ min, max }) => {
-    const myInvalidIds = [];
     const explodedMin = explode(min);
     const explodedMax = explode(max);
+
     if (explodedMin.length < explodedMax.length) {
       explodedMin.unshift(0);
     }
 
-    const len = explodedMin.length / 2;
+    const len = Math.floor(explodedMin.length / 2);
   
-    const numbersToCheck: number[] = [];
-    for (let i = 0; i <= len; i++) {
-      numbersToCheck.push(numberify(take(explodedMin, i+1)))
-      numbersToCheck.push(numberify(take(explodedMax, i+1)))
-    }
-    
-    // Handle the case when the first digits are different, like 2000-5000
-    if (explodedMin[0] < explodedMax[0]) {
-      for (let i = explodedMin[0]; i <= numberify(take(explodedMax, len)); i++) {
-        numbersToCheck.push(i);
+    const numbersToCheck: Set<number> = new Set();
+    const s = arrayToNum(take(explodedMin, len));
+    const e = arrayToNum(take(explodedMax, len + (getDigitCount(max) - getDigitCount(min))));
+
+    for (let i = s; i <= e; i++) {
+      const exploded = explode(i)
+      for (let j = 0; j <= exploded.length; j++) {
+        numbersToCheck.add(arrayToNum(take(exploded, j+1)))
       }
     }
 
-    for (const n of uniq(numbersToCheck)) {
+    for (const n of numbersToCheck) {
       const repeater = doesRepeat(n, min, max);
       if (repeater > 0) {
-        myInvalidIds.push(repeater);
         invalidIDs.push(repeater);
       }
-    }
-    console.log(`min, max`, min, max);
-    for (const n of myInvalidIds.sort()) {
-      console.log(`\t ${n}`)
     }
   });
 
   return sum(uniq(invalidIDs))
 }
 
-function numberify(nums: number[]): number {
+function arrayToNum(nums: number[]): number {
   let number = 0;
   for (const [i, n] of nums.entries()) {
     number += n * Math.pow(10, (nums.length - i - 1));
   }
   return number;
-}
-
-function getRepeats(min: number, max: number): number[] {
-  const repeats = [];
-
-  const minExploded = explode(min);
-  const maxExploded = explode(max);
-
-  if (getDigitCount(min) <= getDigitCount(max)) {
-    minExploded.unshift(0);
-  }
-
-  for (let minI = 0; minI <= getDigitCount(min) / 2; minI++) {
-    const start = parseInt(take(minExploded, minI + 1).join(''));
-    const end = parseInt(take(maxExploded, minI + 1).join(''));
-    for (let p = start; p <= end; p++) {
-      const pR = doesRepeat(p, min, max);
-      if (pR > 0) {
-        repeats.push(pR);
-      }
-    }
-  }
-
-  return repeats;
 }
 
 function explode(num: number): number[] {
@@ -166,10 +135,6 @@ async function getInput(): Promise<Range[]> {
 
 const part1Answer = 28846518423;
 const part2Answer = 31578210022;
-// 31560764177 too low
-// 31561404580 too low
-// 31561483134 too low
-// 31578210022
 
 export {
   part1,
